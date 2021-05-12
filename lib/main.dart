322f74f8f15c8d808de72eaa72cfd5ee4d4e6cad
ShGanesh <54804789+ShGanesh@users.noreply.github.com>
2021-05-13 00:24:35 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
-import 'package:flutter/material.dart';
-import 'package:fsace_detector_app/splashScreen.dart';
+import 'package:flutter/material.dart';                       // importing materials
+import 'package:fsace_detector_app/splashScreen.dart';        // importing splashScreen.dart
 
-void main() {
-  runApp(MyApp());
+void main() {                                                 // Main function, runs MyApp() and ends.
+  runApp(MyApp());                                            // runApp: Inflates the given widget and attaches it to the screen.
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget {                         // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
